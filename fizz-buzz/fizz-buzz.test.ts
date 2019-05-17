5db430e8e9edcf2193ee11b07f769104a58e2140
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-17 10:30:44 -0400

    wrap fizz-buzz test in describe to be able to ignore entire suite

--- fizz-buzz/fizz-buzz.test.ts
+++ fizz-buzz/fizz-buzz.test.ts
@@ -3,20 +3,22 @@ import { FizzBuzz } from "./fizz-buzz";
 const loggerMock = jest.fn();
 const fizzBuzz = new FizzBuzz(loggerMock);
 
-test('fizzBuzz method calls logger with proper parameter', () => {
-    fizzBuzz.fizzBuzz();
-    const results: string[] = loggerMock.mock.calls;
-    const lineBreaks = results[0][0].match(/\n/g);
-    expect(lineBreaks).toHaveLength(99);
-})
+describe ('fizzBuzz kata', () => {
 
-test('generateSequentialNumbers works', () => {
-    const expected = [1, 2, 3, 4, 5];
-    const result = fizzBuzz.generateSequentialNumbers(5);
-    expect(result).toEqual(expected);
-})
+    test('fizzBuzz method calls logger with proper parameter', () => {
+        fizzBuzz.fizzBuzz();
+        const results: string[] = loggerMock.mock.calls;
+        const lineBreaks = results[0][0].match(/\n/g);
+        expect(lineBreaks).toHaveLength(99);
+    })
 
-test.each`
+    test('generateSequentialNumbers works', () => {
+        const expected = [1, 2, 3, 4, 5];
+        const result = fizzBuzz.generateSequentialNumbers(5);
+        expect(result).toEqual(expected);
+    })
+
+    test.each`
 a       | expected
 ${1}    | ${1}
 ${2}    | ${2}
@@ -24,11 +26,11 @@ ${3}    | ${3}
 ${5}    | ${5}
 ${15}   | ${'FizzBuzz'}
 ${30}   | ${'FizzBuzz'}
-`('convertDivisibleBy3And5ToFizzBuzz outputs $expected when passed $a', ({a, expected}) => {
-    expect(fizzBuzz.convertDivisibleBy3And5ToFizzBuzz(a)).toBe(expected);
-})
+`('convertDivisibleBy3And5ToFizzBuzz outputs $expected when passed $a', ({ a, expected }) => {
+        expect(fizzBuzz.convertDivisibleBy3And5ToFizzBuzz(a)).toBe(expected);
+    })
 
-test.each`
+    test.each`
 a               | expected
 ${1}            | ${1}
 ${2}            | ${2}
@@ -37,10 +39,10 @@ ${6}            | ${'Fizz'}
 ${'FizzBuzz'}   | ${'FizzBuzz'}
 ${'Buzz'}       | ${'Buzz'}
 `('convertDivisibleBy3ToFizz outputs $expected when passed $a', ({ a, expected }) => {
-    expect(fizzBuzz.convertDivisibleBy3ToFizz(a)).toBe(expected);
-})
+        expect(fizzBuzz.convertDivisibleBy3ToFizz(a)).toBe(expected);
+    })
 
-test.each`
+    test.each`
 a               | expected
 ${1}            | ${1}
 ${2}            | ${2}
@@ -48,10 +50,10 @@ ${'Fizz'}       | ${'Fizz'}
 ${5}            | ${'Buzz'}
 ${'FizzBuzz'}   | ${'FizzBuzz'}
 `('convertDivisibleBy5ToBuzz outputs $expected when passed $a', ({ a, expected }) => {
-    expect(fizzBuzz.convertDivisibleBy5ToBuzz(a)).toBe(expected);
-})
+        expect(fizzBuzz.convertDivisibleBy5ToBuzz(a)).toBe(expected);
+    })
 
-test.each`
+    test.each`
 a               | expected
 ${1}            | ${1}
 ${2}            | ${2}
@@ -62,10 +64,10 @@ ${'Buzz'}       | ${'Buzz'}
 ${35}           | ${'FizzBuzz'}
 ${53}           | ${'FizzBuzz'}
 `('convertContains3And5ToFizzBuzz outputs $expected when passed $a', ({ a, expected }) => {
-    expect(fizzBuzz.convertContains3And5ToFizzBuzz(a)).toBe(expected);
-})
+        expect(fizzBuzz.convertContains3And5ToFizzBuzz(a)).toBe(expected);
+    })
 
-test.each`
+    test.each`
 a               | expected
 ${1}            | ${1}
 ${2}            | ${2}
@@ -76,10 +78,10 @@ ${'Buzz'}       | ${'Buzz'}
 ${'Fizz'}       | ${'Fizz'}
 ${53}           | ${'Fizz'}
 `('convertContains3ToFizz outputs $expected when passed $a', ({ a, expected }) => {
-    expect(fizzBuzz.convertContains3ToFizz(a)).toBe(expected);
-})
+        expect(fizzBuzz.convertContains3ToFizz(a)).toBe(expected);
+    })
 
-test.each`
+    test.each`
 a               | expected
 ${1}            | ${1}
 ${2}            | ${2}
@@ -90,5 +92,7 @@ ${'Buzz'}       | ${'Buzz'}
 ${'Fizz'}       | ${'Fizz'}
 ${95}           | ${'Buzz'}
 `('convertContains5ToBuzz outputs $expected when passed $a', ({ a, expected }) => {
-    expect(fizzBuzz.convertContains5ToBuzz(a)).toBe(expected);
+        expect(fizzBuzz.convertContains5ToBuzz(a)).toBe(expected);
+    })
+
 })
