df73494a686083b741f567a861dd326c4201abe4
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-19 00:31:03 -0400

    Add final solution to Mine Field kata

--- mine-field/mine-field.ts
+++ mine-field/mine-field.ts
@@ -1,61 +1,49 @@
 export const generateHintField = (input: string): string => {
-    const linesFromInput: string[] = input.split('\n')
-    const lineLength: number = parseInt(linesFromInput[0].split(' ')[1])
-
-    const stringArrayToCharArray = createFlatMapReducerFunc(stringToCharArray)
-    const squareToHint = createSquareToHintMapperFunc(lineLength)
-    const arrayToLines = creatArrayToLinesReducerFunc(lineLength)
-    
-    return linesFromInput
+    return input
+        .split('\n')
         .slice(1)
-        .reduce(stringArrayToCharArray, [])
-        .map(squareToHint)
-        .reduce(arrayToLines)
-}
-
-function createFlatMapReducerFunc<T>(mappingFunction: (x: T) => T[]) {
-    return (previous: T[], current: T) => {
-        return previous.concat(mappingFunction(current))
-    }
+        .map(stringToCharArray)
+        .map(mineFieldRowToHintFieldRow)
+        .reduce(hintFieldArrayToHintFieldString, '')
 }
 
 function stringToCharArray(str: string): string[] {
     return Array.from(str)
 }
 
-function createSquareToHintMapperFunc(lineWidth: number) {
-    return (square: string, index: number, field: string[]): string => {
-        if (square === '*') {
-            return '*'
-        }
-        
-        /** WRONG FORMULAS
-        let count: number =
-            (field[index - lineWidth]       === '*' ? 1 : 0) +
-            (field[index - lineWidth + 1]   === '*' ? 1 : 0) +
-            (field[index + 1]               === '*' ? 1 : 0) +
-            (field[index + lineWidth - 1]   === '*' ? 1 : 0) +
-            (field[index + lineWidth]       === '*' ? 1 : 0) +
-            (field[index + lineWidth - 1]   === '*' ? 1 : 0) +
-            (field[index - 1]               === '*' ? 1 : 0) +
-            (field[index - lineWidth - 1]   === '*' ? 1 : 0)
-        return count.toString()
-        */
-    }
+function mineFieldRowToHintFieldRow(row: string[], rowIndex: number, mineField: string[][]) {
+    const squareToHint = createSquareToHintMappingFunction(mineField, rowIndex)
+    return row.map(squareToHint)
 }
 
-function creatArrayToLinesReducerFunc(lineWidth: number) {
-    return (output: string, square: string, index: number): string => {
-        if (index % lineWidth === 0) {
-            return output + '\n' + square;
-        }
-        return output + square;
+function hintFieldArrayToHintFieldString(hintString: string, row: string[], rowIndex: number, hintField: string[][]) {
+    let line = hintString + row.join('')
+    if (rowIndex < hintField.length - 1) {
+        line += '\n'
+    }
+    return line
+}
+
+function createSquareToHintMappingFunction(mineField: string[][], rowIndex: number) {
+    return (square: string, columnIndex: number) => {
+        return square === '*' ? '*' : getNumberOfAdjacentMineSquares(mineField, rowIndex, columnIndex)
     };
 }
 
-generateHintField(
-`3 4
-*...
-..*.
-....`
-) //?
+function getNumberOfAdjacentMineSquares(mineField: string[][], rowIndex: number, columnIndex: number) {
+    const squareHasMine = (rowIndex: number, columnIndex: number): boolean => {
+        return mineField[rowIndex] != undefined && mineField[rowIndex][columnIndex] === '*';
+    }
+
+    let count: number = 
+        +squareHasMine(rowIndex - 1, columnIndex) +
+        +squareHasMine(rowIndex - 1, columnIndex + 1) +
+        +squareHasMine(rowIndex, columnIndex + 1) +
+        +squareHasMine(rowIndex + 1, columnIndex + 1) +
+        +squareHasMine(rowIndex + 1, columnIndex) +
+        +squareHasMine(rowIndex + 1, columnIndex - 1) +
+        +squareHasMine(rowIndex, columnIndex - 1) +
+        +squareHasMine(rowIndex - 1, columnIndex - 1);
+    return count.toString();
+}
+
