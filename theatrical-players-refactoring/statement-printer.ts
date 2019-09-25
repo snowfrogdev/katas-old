4e5fb66a94771697a5f1ff32c769c00402908e33
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2019-09-25 09:02:03 -0400

    [Refactor] Add PlayType enum to remove magic strings.

--- theatrical-players-refactoring/statement-printer.ts
+++ theatrical-players-refactoring/statement-printer.ts
@@ -1,8 +1,11 @@
-import { performance } from 'perf_hooks';
-
 interface Play {
   name: string;
-  type: string;
+  type: PlayType;
+}
+
+enum PlayType {
+  Tragedy = "tragedy",
+  Comedy = "comedy",
 }
 
 interface Performance {
@@ -51,13 +54,13 @@ export class StatementPrinter {
     let amount = 0;
     const playType = this.getPlayById(performance.playID).type;
     switch (playType) {
-      case 'tragedy':
+      case PlayType.Tragedy:
         amount = 400_00;
         if (performance.audience > 30) {
           amount += 10_00 * (performance.audience - 30);
         }
         break;
-      case 'comedy':
+      case PlayType.Comedy:
         amount = 300_00;
         if (performance.audience > 20) {
           amount += 100_00 + 5_00 * (performance.audience - 20);
@@ -73,7 +76,7 @@ export class StatementPrinter {
   private calculatePerformanceVolumeCredits(performance: Performance): number {
     let volumeCredits = 0;
     volumeCredits += Math.max(performance.audience - 30, 0);
-    if ('comedy' === this.getPlayById(performance.playID).type)
+    if (PlayType.Comedy === this.getPlayById(performance.playID).type)
       volumeCredits += Math.floor(performance.audience / 5);
     return volumeCredits;
   }
