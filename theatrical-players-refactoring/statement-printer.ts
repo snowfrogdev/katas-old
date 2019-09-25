5c706e9af1b47dc3488499ce8b2c8255af3e7e81
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2019-09-24 20:54:55 -0400

    [Refactor] Rename `thisAmount` to `amount`

--- theatrical-players-refactoring/statement-printer.ts
+++ theatrical-players-refactoring/statement-printer.ts
@@ -26,24 +26,24 @@ export class StatementPrinter {
   }
 
   private calculateAmount(play: any, performance: any) {
-    let thisAmount = 0;
+    let amount = 0;
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40000;
+        amount = 40000;
         if (performance.audience > 30) {
-          thisAmount += 1000 * (performance.audience - 30);
+          amount += 1000 * (performance.audience - 30);
         }
         break;
       case 'comedy':
-        thisAmount = 30000;
+        amount = 30000;
         if (performance.audience > 20) {
-          thisAmount += 10000 + 500 * (performance.audience - 20);
+          amount += 10000 + 500 * (performance.audience - 20);
         }
-        thisAmount += 300 * performance.audience;
+        amount += 300 * performance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    return thisAmount;
+    return amount;
   }
 }
