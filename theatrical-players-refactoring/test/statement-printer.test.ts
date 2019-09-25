34bb96ed2cc449e1a4c6a3264f8a276eb353a6f2
Philippe Vaillancourt <philippe_vaillancourt@sympatico.ca>
2019-09-24 21:06:01 -0400

    [Refactor] Change test to import JSON as modules instead of using Node's filesystem API.

--- theatrical-players-refactoring/test/statement-printer.test.ts
+++ theatrical-players-refactoring/test/statement-printer.test.ts
@@ -1,24 +1,17 @@
 import { StatementPrinter } from "../statement-printer";
-const fs = require('fs');
+import * as invoice from "./invoice.json";
+import * as plays from "./plays.json";
+import * as newInvoice from "./invoice_new_plays.json";
+import * as newPlays from "./new_plays.json";
 
 const printer = new StatementPrinter();
 
 test('example statement', () => {
-  const invoice = JSON.parse(fs.readFileSync('theatrical-players-refactoring/test/invoice.json', 'utf8'));
-  const plays = JSON.parse(
-    fs.readFileSync('theatrical-players-refactoring/test/plays.json', 'utf8')
-  );
   expect(printer.print(invoice, plays)).toMatchSnapshot();
 });
 
 test('statement with new play types', () => {
-  const invoice = JSON.parse(
-    fs.readFileSync('theatrical-players-refactoring/test/invoice_new_plays.json', 'utf8')
-  );
-  const plays = JSON.parse(
-    fs.readFileSync('theatrical-players-refactoring/test/new_plays.json', 'utf8')
-  );
   expect(() => {
-    printer.print(invoice, plays);
+    printer.print(newInvoice, newPlays);
   }).toThrow(/unknown type/);
 });
